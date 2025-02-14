ffffb641353925dc69f83e39181acea9d1e80e24
ahmad afandi <ahmad.afandi85@gmail.com>
2025-02-14 07:04:00 +0700

    sesuaikan model penduduk karena tabel tweb_sakit_menahun telah dihapus

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\SakitMenahunEnum;
 use App\Models\Traits\FilterWilayahTrait;
 use App\Models\Traits\QueryTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -64,6 +65,7 @@ class Penduduk extends BaseModel
         'statusHamil',
         'namaAsuransi',
         'umur',
+        'namaSakitMenahun',
         'tanggalLahirId',
         'urlFoto',
     ];
@@ -163,17 +165,7 @@ class Penduduk extends BaseModel
     public function cacat()
     {
         return $this->belongsTo(Cacat::class, 'cacat_id')->withDefault();
-    }
-
-    /**
-     * Define an inverse one-to-one or many relationship.
-     *
-     * @return BelongsTo
-     */
-    public function sakitMenahun()
-    {
-        return $this->belongsTo(SakitMenahun::class, 'sakit_menahun_id')->withDefault();
-    }
+    }    
 
     /**
      * Define an inverse one-to-one or many relationship.
@@ -347,6 +339,12 @@ class Penduduk extends BaseModel
         };
     }
 
+    public function getNamaSakitMenahunAttribute()
+    {
+        $sakitMenahunId = $this->sakit_menahun_id ?? SakitMenahunEnum::TIDAK_ADA_TIDAK_SAKIT;
+        return SakitMenahunEnum::getDescription($sakitMenahunId);
+    }
+
     /**
      * Getter wajib ktp attribute.
      *
@@ -507,8 +505,7 @@ class Penduduk extends BaseModel
             'pekerjaan',
             'wargaNegara',
             'golonganDarah',
-            'cacat',
-            'sakitMenahun',
+            'cacat',            
             'kb',
             'statusKawin',
             'statusRekamKtp',
