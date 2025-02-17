977c32ccd819eea6db1afc8f82b2c914ca6bd0b4
FirlianiF <firlianif@gmail.com>
2025-02-17 15:51:57 +0700

    update

--- app/Models/Point.php
+++ app/Models/Point.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Point extends BaseModel
 {
+    use HasFactory;
     public const LOCK = 1;
 
     public const UNLOCK = 2;
