8b934786e3593428d188a2babdcaf125963cb169
ahmad afandi <ahmad.afandi85@gmail.com>
2025-05-26 10:08:14 +0700

    update komentar

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -471,6 +471,7 @@ class Penduduk extends BaseModel
     public function scopeStatus($query, $value = 1, $tanggalPeristiwa = null, $configDesa = null)
     {
         if($value != StatusDasarEnum::MATI) {
+            // kalau log_penduduk lengkap, seharusnya tidak perlu join dengan log_penduduk terakhir, jika tanggal_peristiwa null
             $logPenduduk = LogPenduduk::select(['log_penduduk.id_pend'])->peristiwaTerakhir($tanggalPeristiwa, $configDesa)->tidakMati()->toBoundSql();
             return $query->where('status_dasar', $value)->join(DB::raw("($logPenduduk) as log"), 'log.id_pend', '=', 'tweb_penduduk.id');
         }
